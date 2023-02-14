47c98aa7bfd82d4da7bd22a6f759b7135e862757
Samuel Harpaz <sharpaz24@gmail.com>
2023-02-13 21:14:30 -0500

    add more methods to todolist class

--- todolist.js
+++ todolist.js
@@ -101,9 +101,79 @@ class TodoList {
     return `${title}\n${list}`;
   }
 
+  forEach(callback) {
+    this.todos.forEach(callback);
+  }
+
+  filter(callback) {
+    const newList = new TodoList(this.title);
+
+    this.forEach(todo => {
+      if (callback(todo)) {
+        newList.add(todo);
+      }
+    });
+
+    return newList;
+  }
+
+  findByTitle(title) {
+    return this.filter(todo => todo.getTitle() === title).first();
+  }
+
+  allDone() {
+    return this.filter(todo => todo.isDone());
+  }
+
+  allNotDone() {
+    return this.filter(todo => !todo.isDone());
+  }
+
+  markDone(title) {
+    const todo = this.findByTitle(title);
+
+    if (todo) {
+      todo.markDone();
+    }
+  }
+
+  markAllDone() {
+    this.forEach(todo => todo.markDone());
+  }
+
+  markAllUndone() {
+    this.forEach(todo => todo.markUndone());
+  }
+
+  toArray() {
+    return [...this.todos];
+  }
+
   _validateIndex(index) {
     if (!(index in this.todos)) {
       throw new ReferenceError(`invalid index: ${index}`);
     }
   }
 }
+
+let todo1 = new Todo("Buy milk");
+let todo2 = new Todo("Clean room");
+let todo3 = new Todo("Go to the gym");
+let todo4 = new Todo("Go shopping");
+let todo5 = new Todo("Feed the cats");
+let todo6 = new Todo("Study for Launch School");
+let list = new TodoList("Today's Todos");
+
+list.add(todo1);
+list.add(todo2);
+list.add(todo3);
+list.add(todo4);
+list.add(todo5);
+list.add(todo6);
+todo1.markDone();
+todo5.markDone();
+
+// let doneTodos = list.filter(todo => todo.isDone());
+// console.log(doneTodos);
+
+console.log(list.toArray() === list.todos);
